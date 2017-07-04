8d3d1d213802ac30e960c3389a025464a2ca4a7e
zhfkt <zhfkt@hotmail.com>
2017-07-04 17:55:14 +0800

    Create graphUtil and util class

--- ComplexCi/ComplexCi.cpp
+++ ComplexCi/ComplexCi.cpp
@@ -19,199 +19,152 @@
 #include <bitset>
 
 using namespace std;
-using namespace std::chrono;
+using namespace chrono;
 
 
-template<typename Out>
-void split(const std::string &s, char delim, Out result) {
-	std::stringstream ss;
-	ss.str(s);
-	std::string item;
-	while (std::getline(ss, item, delim)) {
-		*(result++) = item;
+
+
+class util
+{
+private:
+	util(){}
+
+public:
+
+	template<typename Out>
+	static void split(const string &s, char delim, Out result) {
+		stringstream ss;
+		ss.str(s);
+		string item;
+		while (getline(ss, item, delim)) {
+			*(result++) = item;
+		}
 	}
-}
+
+	static vector<string> split(const string &s, char delim) {
+		vector<string> elems;
+		split(s, delim, back_inserter(elems));
+		return elems;
+	}
+
+};
 
 
-std::vector<std::string> split(const std::string &s, char delim) {
-	std::vector<std::string> elems;
-	split(s, delim, std::back_inserter(elems));
-	return elems;
-}
-
-/*void getNeighbourFrontierAndScope(const vector<vector<int> > &adjListGraph, int scope, int currentNode, unordered_set<int> &currentSet, unordered_set<int>& alreadyAccess)
+class graphUtil
 {
-//alreadyAccess.reserve(100000);
+private:
 
-currentSet.insert(currentNode);
-alreadyAccess.insert(currentNode);
+	vector<bool> alreadyAccessBool;
+	vector<int> bfsQueue;
+	int startIt;
+	int endIt;
 
-for (int i = 0; i < scope; i++)
-{
-unordered_set<int> nextSet;
-//nextSet.reserve(30000);
-
-for (const auto& node : currentSet)
-{
-const vector<int>& neighbourNodeList = adjListGraph[node];
-
-for (const auto& eachNeighbour : neighbourNodeList)
-{
-if (alreadyAccess.find(eachNeighbour) == alreadyAccess.end())
-{
-nextSet.insert(eachNeighbour);
-alreadyAccess.insert(eachNeighbour);
-}
-}
-
-}
-
-currentSet = move(nextSet);
-}
-}*/
-
-vector<bool> alreadyAccessBool;
-vector<int> bfsQueue;
-int startIt = 0;
-int endIt = 1;
-
-void getNeighbourFrontierAndScope(const vector<vector<int> > &adjListGraph, int scope, int currentNode)
-{
-	startIt = 0;
-	endIt = 1;
-
-	//startIt and endIt will never execeed bfsQueue.size();
-	//alreadyAccess is between 0 and endIt
-	//fontier is between startIt and endIt
-
-	bfsQueue[0] = currentNode;
-	alreadyAccessBool[currentNode] = 1;
-
-	for (int i = 0; i < scope; i++)
+public:
+	graphUtil(int totalSize) : startIt(0),endIt(1)
 	{
-		int lastEndIt = endIt;
-		while (startIt != lastEndIt)
-		{
-			const vector<int>& neighbourNodeList = adjListGraph[bfsQueue[startIt++]];
+		bfsQueue.resize(totalSize, -1);
+		alreadyAccessBool.resize(totalSize, 0);	
+	}
 
-			for (const auto& eachNeighbour : neighbourNodeList)
+	vector<int>& getBfsQueue()
+	{
+		return bfsQueue;
+	}
+
+	int getEndIt()
+	{
+		return endIt;
+	}
+
+
+	void getNeighbourFrontierAndScope(const vector<vector<int> > &adjListGraph, int scope, int currentNode)
+	{
+		startIt = 0;
+		endIt = 1;
+
+		//startIt and endIt will never execeed bfsQueue.size();
+		//alreadyAccess is between 0 and endIt
+		//fontier is between startIt and endIt
+
+		bfsQueue[0] = currentNode;
+		alreadyAccessBool[currentNode] = 1;
+
+		for (int i = 0; i < scope; i++)
+		{
+			int lastEndIt = endIt;
+			while (startIt != lastEndIt)
 			{
-				if (!alreadyAccessBool[eachNeighbour])
+				const vector<int>& neighbourNodeList = adjListGraph[bfsQueue[startIt++]];
+
+				for (const auto& eachNeighbour : neighbourNodeList)
 				{
-					bfsQueue[endIt++] = eachNeighbour;
-					alreadyAccessBool[eachNeighbour] = 1;
+					if (!alreadyAccessBool[eachNeighbour])
+					{
+						bfsQueue[endIt++] = eachNeighbour;
+						alreadyAccessBool[eachNeighbour] = 1;
+					}
 				}
 			}
 		}
-	}
-	
-	for (int i = 0; i < endIt; i++)
-	{
-		alreadyAccessBool[bfsQueue[i]] = 0;
-	}
 
-
-
-}
-
-
-
-long long basicCi(const vector<vector<int> > &adjListGraph, int ballRadius, int currentNode)
-{
-	if (adjListGraph[currentNode].size() == 0)
-	{
-		return -1;
+		for (int i = 0; i < endIt; i++)
+		{
+			alreadyAccessBool[bfsQueue[i]] = 0;
+		}
 	}
 
-	if (ballRadius == 0)
+
+
+	long long basicCi(const vector<vector<int> > &adjListGraph, int ballRadius, int currentNode)
 	{
-		return adjListGraph[currentNode].size();
+		if (adjListGraph[currentNode].size() == 0)
+		{
+			return -1;
+		}
+
+		if (ballRadius == 0)
+		{
+			return adjListGraph[currentNode].size();
+		}
+
+		getNeighbourFrontierAndScope(adjListGraph, ballRadius, currentNode);
+
+		long long ci = 0;
+
+		for (int i = startIt; i < endIt; i++)
+		{
+			ci += (adjListGraph[bfsQueue[i]].size() - 1);
+		}
+
+		ci *= (adjListGraph[currentNode].size() - 1);
+
+		return ci;
 	}
 
-	getNeighbourFrontierAndScope(adjListGraph, ballRadius, currentNode);
-
-	long long ci = 0;
-
-	for (int i = startIt; i < endIt; i++)
+	void deleteNode(vector<vector<int> > &adjListGraph, int node)
 	{
-		ci += (adjListGraph[bfsQueue[i]].size() - 1);
+		/*
+		if (adjListGraph[node].size() == 0)
+		{
+		return;
+		} // not must here
+		*/
+
+		for (auto neighbour : adjListGraph[node])
+		{
+			adjListGraph[neighbour].erase(remove(adjListGraph[neighbour].begin(), adjListGraph[neighbour].end(), node), adjListGraph[neighbour].end());
+		}
+
+		adjListGraph[node].clear();
+
 	}
 
-	ci *= (adjListGraph[currentNode].size() - 1);
-
-
-	/*
-	if (currentNode % 500 == 0)
-	{
-	cout << currentNode << " currentFrontier size :" << currentFrontier.size() << endl;
-	}
-	*/
-
-	/*
-	if (currentNode == 1715132 && (ci == 0 || ci == 2))
-	{
-	cout << "currentNode: " << currentNode << " adjListGraph[currentNode].size(): " << adjListGraph[currentNode].size() << "/ ";
-
-
-	for (auto node : adjListGraph[currentNode])
-	{
-	cout << node << " " ;
-	}
-
-	cout << endl;
-
-
-	cout << "ci: " << ci << endl;
-
-	cout << "currentFrontier: " << endl;
-
-	for (auto node : currentFrontier)
-	{
-	cout << node << " " << adjListGraph[node].size() << endl;
-	}
-
-	cout << "dummyValue: " << endl;
-
-	for (auto node : dummyValue)
-	{
-	cout << node << " node1: ";
-
-	for (auto node1 : adjListGraph[node])
-	{
-	cout <<  node1 << " ";
-	}
-	cout << endl;
-	}
-	cout << endl;
-
-	}
-	*/
-
-	return ci;
-}
+};
 
 
 
 
 
-
-void deleteNode(vector<vector<int> > &adjListGraph, int node)
-{
-	/*
-	if (adjListGraph[node].size() == 0)
-	{
-	return;
-	} // not must here
-	*/
-
-	for (auto neighbour : adjListGraph[node])
-	{
-		adjListGraph[neighbour].erase(remove(adjListGraph[neighbour].begin(), adjListGraph[neighbour].end(), node), adjListGraph[neighbour].end());
-	}
-
-	adjListGraph[node].clear();
-
-}
 
 int main(int argc, char* argv[])
 {
@@ -245,20 +198,20 @@ int main(int argc, char* argv[])
 	string modelID = "";
 
 	{
-		vector<string> fileName = split(path, '/');
-		modelID = split(fileName[fileName.size() - 1], '.')[0];
+		vector<string> fileName = util::split(path, '/');
+		modelID = util::split(fileName[fileName.size() - 1], '.')[0];
 	}
 
-	std::cout << "path: " << path << endl;
-	std::cout << "output: " << output << endl;
-	std::cout << "modelID: " << modelID << endl;
+	cout << "path: " << path << endl;
+	cout << "output: " << output << endl;
+	cout << "modelID: " << modelID << endl;
 
-	std::cout << "ballRadius: " << ballRadius << endl;
-	std::cout << "updateBatch: " << updateBatch << endl;
-	std::cout << "outputNumBatch: " << outputNumBatch << endl;
+	cout << "ballRadius: " << ballRadius << endl;
+	cout << "updateBatch: " << updateBatch << endl;
+	cout << "outputNumBatch: " << outputNumBatch << endl;
 
 
-	std::cout << "First Read Start" << endl;
+	cout << "First Read Start" << endl;
 
 	unordered_set<int> allVex;
 	string eachLine;
@@ -267,7 +220,7 @@ int main(int argc, char* argv[])
 
 	while (is >> eachLine)
 	{
-		vector<string> csvEachLine = split(eachLine, ',');
+		vector<string> csvEachLine = util::split(eachLine, ',');
 
 		allVex.insert(stoi(csvEachLine[0]));
 		allVex.insert(stoi(csvEachLine[1]));
@@ -276,38 +229,36 @@ int main(int argc, char* argv[])
 	is.close();
 	int totalSize = allVex.size();
 
-	std::cout << "First Read End/Second Read Start" << endl;
+	cout << "First Read End/Second Read Start" << endl;
 
 
 	vector<vector<int> > adjListGraph(totalSize);
 	is.open(path);
 	while (is >> eachLine)
 	{
-		vector<string> csvEachLine = split(eachLine, ',');
+		vector<string> csvEachLine = util::split(eachLine, ',');
 
 		adjListGraph[stoi(csvEachLine[0])].push_back(stoi(csvEachLine[1]));
 		adjListGraph[stoi(csvEachLine[1])].push_back(stoi(csvEachLine[0]));
 	}
 
-	std::cout << "Second Read End" << endl;
-
-	bfsQueue.resize(totalSize, -1);
-	alreadyAccessBool.resize(totalSize, 0);
+	cout << "Second Read End" << endl;
 
 	//--------------
 
 
-
-
 	set<pair<long long, int> > allPQ; //ci/currentNode --- long is 32 bit on the win and long long is 64 bit / and long long can be multiple
 	vector<long long> revereseLoopUpAllPQ(totalSize);
+
+	graphUtil gu(totalSize);
+
 	cout << "modelID: " << modelID << " First Cal CI" << endl;
 
 	for (int i = 0; i < adjListGraph.size(); i++)
 	{
 		int currentNode = i;
 		// core_ci
-		long long ci = basicCi(adjListGraph, ballRadius, currentNode);
+		long long ci = gu.basicCi(adjListGraph, ballRadius, currentNode);
 
 		allPQ.insert(make_pair(ci, currentNode));
 		revereseLoopUpAllPQ[currentNode] = ci;
@@ -349,23 +300,12 @@ int main(int argc, char* argv[])
 			finalOutput.push_back(rit->second);
 			allVex.erase(rit->second);  //remove key
 		}
-
-		/*
-		if (loopCount == 600000)
-		{
-		cout << " revereseLoopUpAllPQ[1715132] " << revereseLoopUpAllPQ[1715132] << endl;
-		}
-		*/
-
-
-		//cout << "monitor 1" << endl;
-
-		//int debugCount = 0;
-
 		
 		for (int i : batchList)
 		{
-			getNeighbourFrontierAndScope(adjListGraph, ballRadius + 1, i);
+			gu.getNeighbourFrontierAndScope(adjListGraph, ballRadius + 1, i);
+			const vector<int>& bfsQueue = gu.getBfsQueue();
+			int endIt = gu.getEndIt();
 
 			for (auto bfsIt = bfsQueue.begin(); bfsIt != bfsQueue.begin() + endIt; bfsIt++)
 			{
@@ -375,28 +315,18 @@ int main(int argc, char* argv[])
 					candidateUpdateNodesBool[*bfsIt] = 1;
 				}
 			}
-			//cout << "monitor 1_2: " << debugCount++ << " " << batchList.size() << " " << candidateUpdateNodes.size() << endl;
-
 		}
 
-
-
-
-
-		//cout << "monitor 2" << endl;
 
 		for (int i : batchList)
 		{
-			deleteNode(adjListGraph, i);
+			gu.deleteNode(adjListGraph, i);
 			//candidateUpdateNodesWithCi.insert(make_pair(i, -1));// no need to because self will be included in the candidateUpdateNodes and updated in the below
 		}
 
-		//cout << "monitor 3" << endl;
-		//debugCount = 0;
-
 		for (auto canIt = candidateUpdateNodesVector.begin(); canIt != (candidateUpdateNodesVector.begin() + candidateEnd); canIt++)
 		{
-			long long updatedCi = basicCi(adjListGraph, ballRadius, *canIt);
+			long long updatedCi = gu.basicCi(adjListGraph, ballRadius, *canIt);
 
 			long long olderCi = revereseLoopUpAllPQ[*canIt];
 
@@ -407,16 +337,7 @@ int main(int argc, char* argv[])
 
 			candidateUpdateNodesBool[*canIt] = 0; //recover candidateUpdateNodesBool to zero
 
-			//cout << "monitor 3_4: " << debugCount++ << " " << candidateUpdateNodes.size() << endl;
 		}
-		//cout << "monitor 4" << endl;
-
-		/*if ((debugPreviousMax.first) < (allPQ.rbegin()->first))
-		{
-		cout << "monitor 5: " << debugPreviousMax.first << " " << allPQ.rbegin()->first << endl;;
-		}
-		*/
-
 
 	}
 
@@ -443,12 +364,9 @@ CIEND:
 		}
 	}
 
-	//delete[] alreadyAccessBool;
-	//delete[] candidateUpdateNodesBool;
-
 	//--------------
 
-	std::cout << "Outputing Start.." << endl;
+	cout << "Outputing Start.." << endl;
 
 	ofstream os(output);
 
@@ -464,7 +382,7 @@ CIEND:
 
 		if (finalOutput[i] != -1)
 		{
-			output500 += (',' + std::to_string(finalOutput[i]));
+			output500 += (',' + to_string(finalOutput[i]));
 		}
 		else
 		{
@@ -474,7 +392,6 @@ CIEND:
 		if (i % outputNumBatch == (outputNumBatch - 1) || i == (finalOutput.size() - 1))
 		{
 			os << output500 << endl;
-			//std::cout << "output500: " << output500 << endl;
 			output500.clear();
 		}
 
@@ -483,7 +400,7 @@ CIEND:
 
 	os.close();
 
-	std::cout << "Outputing End.." << endl;
+	cout << "Outputing End.." << endl;
 
 	high_resolution_clock::time_point t2 = high_resolution_clock::now();
 	auto duration = duration_cast<seconds>(t2 - t1).count();
