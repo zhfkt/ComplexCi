93476d2323287e4c5040f5bb086b9152273c6d67
zhfkt <zhfkt@hotmail.com>
2017-07-08 13:46:12 +0800

    add warning option

--- bin/make.sh
+++ bin/make.sh
@@ -1,1 +1,1 @@
-git pull; /scratch/fengkzhu/develop/owndev/ComplexCi/software/gccInstall71/bin/g++ ../ComplexCi/ComplexCi.cpp ./ComplexCi/CI_HEAP.c   -o ComplexCi -O3 ;
+git pull; /scratch/fengkzhu/develop/owndev/ComplexCi/software/gccInstall71/bin/g++ ../ComplexCi/ComplexCi.cpp ./ComplexCi/CI_HEAP.c   -o ComplexCi -O3 -Wall ;
