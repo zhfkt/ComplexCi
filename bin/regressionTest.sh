9ec30356bd30538ce6afb81ace1cd02638f92f71
zhfkt <zhfkt@hotmail.com>
2017-08-06 22:41:19 +0800

    Correct the regex matching for the serId

--- bin/regressionTest.sh
+++ bin/regressionTest.sh
@@ -5,7 +5,7 @@ do
 	#sleep 5s
 	logFile=`ls -t | head -1`
 	tail $logFile
-	serId=`echo $logFile | sed -e 's/\..*$//'`
+	serId=`echo $logFile | sed -e 's/\.log$//'`
 	cd ../Master_algorithm
 	groovy  -cp 'target/algorithm-1.0-SNAPSHOT.jar:/root/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar'  src/main/java/org/dc/algorithm/NetMaster.groovy  ../data/networks/results/$serId/$serId.csv  ../data/networks.zip
 	cd -
