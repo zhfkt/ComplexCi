8a22a5cb5875d1fe2c00b020a9d7ed2d9127e002
zhfkt <zhfkt@hotmail.com>
2017-08-14 01:11:55 +0800

    Add more data in the paperData.sh

--- bin/experimentResult.sh
+++ bin/experimentResult.sh
@@ -12,7 +12,7 @@ fi
 
 serIDRegression=$(date "+%y_%m_%d_%H_%M_%S")_"$ballRadius"_"$batch"_"$outputBatch"_"$method"_"$reInsertRatio"
 regressionResult=`readlink -f regressionResult/$serIDRegression.csv`
-echo " , model1 , model2 , model3 , model4 , real1 , real2 , real3 , real4 , total " > $regressionResult
+echo $regressionResult
 
 date
 
--- bin/paperData.sh
+++ bin/paperData.sh
@@ -2,23 +2,66 @@ serIDRegression="paperData"_$(date "+%y_%m_%d_%H_%M_%S")
 regressionResult=`readlink -f regressionResult/$serIDRegression.csv`
 echo $regressionResult
 
-for i in 5 7 8 1 13
+for i in 0 1 2 5 7 8 9 10
 do
 	echo Now $i
 	for j in 0 1 2
 	do
-		
-		ratio=0.001
-		
-		if((i==13))
-		then
-			ratio=0.01
-		fi
-	
-		./executeAll.sh $j 1 500 $i $ratio
+		./executeAll.sh $j 1 500 $i 0.001
 		./calGroovyBenchmark.sh $regressionResult
 	done
 done
+
+for i in 9 10
+do
+	echo Now $i
+	for j in 0 1 2
+	do
+		./executeAll.sh $j 500 500 $i 0.01
+		./calGroovyBenchmark.sh $regressionResult
+	done
+done
+
+for i in 9 10
+do
+	echo Now $i
+	for j in 0 1 2
+	do
+		./executeAll.sh $j 1 500 $i 0.01
+		./calGroovyBenchmark.sh $regressionResult
+	done
+done
+
+for i in 9 10
+do
+	echo Now $i
+	for j in 0 1 2
+	do
+		./executeAll.sh $j 1 500 $i 0.0
+		./calGroovyBenchmark.sh $regressionResult
+	done
+done
+
+for i in 11 12 13 14
+do
+	echo Now $i
+	for j in 0 1 2
+	do
+		./executeAll.sh $j 1 500 $i 0.001
+		./calGroovyBenchmark.sh $regressionResult
+	done
+done
+
+for i in 11 12 13 14
+do
+	echo Now $i
+	for j in 0 1 2
+	do
+		./executeAll.sh $j 1 500 $i 0.0
+		./calGroovyBenchmark.sh $regressionResult
+	done
+done
+
 
 ./bestResult.sh
 ./calGroovyBenchmark.sh $regressionResult
