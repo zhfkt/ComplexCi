ca5b19a445e0526af4dc68b79f9b9d27f7de023f
zhfkt <zhfkt@hotmail.com>
2017-08-14 11:44:25 +0800

    add the test case entry in the experimentResult.sh

--- bin/experimentResult.sh
+++ bin/experimentResult.sh
@@ -1,22 +1,16 @@
-if (( $# >= 5 ))
-then
-    ballRadius=$1
-    batch=$2
-    outputBatch=$3
-	method=$4
-	reInsertRatio=$5
-else
-    echo './experimentResult.sh 2 500 500 0 0.001'
-    exit;
-fi
-
-serIDRegression=$(date "+%y_%m_%d_%H_%M_%S")_"$ballRadius"_"$batch"_"$outputBatch"_"$method"_"$reInsertRatio"
+serIDRegression="experimentResult"_$(date "+%y_%m_%d_%H_%M_%S")
 regressionResult=`readlink -f regressionResult/$serIDRegression.csv`
 echo $regressionResult
 
 date
 
-./executeAll.sh $ballRadius $batch $outputBatch $method $reInsertRatio
-./calGroovyBenchmark.sh $regressionResult
+for i in 7 8
+do
+	echo Now $i
+	
+	./executeAll.sh 2 1 500 $i 0.001
+	./calGroovyBenchmark.sh $regressionResult
+
+done
 
 date
