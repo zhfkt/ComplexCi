91376a55f252d4ef82add9f9f02f3237ee2a5aea
zhfkt <zhfkt@hotmail.com>
2017-08-20 15:54:42 +0800

    fix for the win scripts bestResult.cmd

--- x64/Release/bestResult.cmd
+++ x64/Release/bestResult.cmd
@@ -1,7 +1,7 @@
-ComplexCi ../../data/networks/model1.csv 2 1 500 18 0.01 
-ComplexCi ../../data/networks/model2.csv 2 1 500 18 0.01 
+ComplexCi ../../data/networks/model1.csv 2 1 500 18 0.001 
+ComplexCi ../../data/networks/model2.csv 2 1 500 18 0.001 
 ComplexCi ../../data/networks/model3.csv 2 1 500 15 0.001  
-ComplexCi ../../data/networks/model4.csv 2 1 500 18 0.01
+ComplexCi ../../data/networks/model4.csv 2 1 500 18 0.001
 ComplexCi ../../data/networks/real1.csv 2 1 500 14 0.001  
 ComplexCi ../../data/networks/real2.csv 2 1 500 18 0.001  
 ComplexCi ../../data/networks/real3.csv 2 1 500 14 0.001  
