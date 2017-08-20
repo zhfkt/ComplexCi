bab7987b9acb312ee0d232ad5559e69904592a31
zhfkt <zhfkt@hotmail.com>
2017-08-20 15:47:45 +0800

    add windows cmd script

---
+++ x64/Release/quickResult.cmd
@@ -0,0 +1,8 @@
+start ComplexCi ../../data/networks/model1.csv 0 1 500 10 0.01 
+start ComplexCi ../../data/networks/model2.csv 0 1 500 10 0.01 
+start ComplexCi ../../data/networks/model3.csv 0 1 500 9 0.01  
+start ComplexCi ../../data/networks/model4.csv 0 1 500 10 0.01
+start ComplexCi ../../data/networks/real1.csv 0 1 500 10 0.01  
+start ComplexCi ../../data/networks/real2.csv 0 1 500 10 0.01  
+start ComplexCi ../../data/networks/real3.csv 0 1 500 10 0.01  
+start ComplexCi ../../data/networks/real4.csv 0 1 500 10 0.01  
