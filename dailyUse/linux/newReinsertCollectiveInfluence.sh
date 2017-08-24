5ade6e9f1b846697333d2dbc6b090a2607d015f5
zhfkt <zhfkt@hotmail.com>
2017-08-24 17:59:54 +0800

    add easy use linux script

---
+++ dailyUse/linux/newReinsertCollectiveInfluence.sh
@@ -0,0 +1,18 @@
+if (( $# >= 4 ))
+then
+	networkPath=$1
+    ballRadius=$2
+	batchSize=$3
+	whetherOutputLimitedNode=$4
+else
+    echo './newReinsertCollectiveInfluence.sh <networkPath> <ballRadius> <batchSize> <whetherOutputLimitedNode>'
+	echo '<ballRadius> is the Radius parameter defined in the Collective Influence Algorithm'
+	echo '<batchSize> batch size of deleted points per updating Collective Influence value'
+	echo '<whetherOutputLimitedNode> whether output limited point leading to 0.01 of giant component ratio or all points'
+	echo './newReinsertCollectiveInfluence.sh /home/network/model1.csv 2 500 1'
+	
+    exit;
+fi
+
+
+../../bin/ComplexCi $networkPath $ballRadius $batchSize 500 10 0.01 $whetherOutputLimitedNode
